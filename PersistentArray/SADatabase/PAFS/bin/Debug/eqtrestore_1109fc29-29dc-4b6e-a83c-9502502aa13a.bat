1c48b34559c76fe8bce17403ef67b58606951035
Glen Watson <gwatson@student.neumont.edu>
2012-05-29 10:32:36 -0400

    removed **/obj/ **/bin/ TestResults/ *.user (** = any amount of directories, whereas * = only one directory)

--- PersistentArray/SADatabase/PAFS/bin/Debug/eqtrestore_1109fc29-29dc-4b6e-a83c-9502502aa13a.bat
+++
@@ -1,4 +0,0 @@
-REM This file restores the in-place instrumented code coverage binaries
-
-
-COPY EQT_1109fc29-29dc-4b6e-a83c-9502502aa13a\PAFS.dll . 
--- PersistentArray/SADatabase/PLL/bin/Debug/eqtrestore_8ac25fab-3e9b-4a06-bce3-56e6a5aeefc9.bat
+++
@@ -1,4 +0,0 @@
-REM This file restores the in-place instrumented code coverage binaries
-
-
-COPY EQT_8ac25fab-3e9b-4a06-bce3-56e6a5aeefc9\PLL.dll . 
