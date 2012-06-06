23de8effb6e15ac87eebd5def630c4f16d2b5f05
Glen Watson <gwatson@student.neumont.edu>
2012-06-06 16:45:35 -0400

    stubbed out project for Distributed Tests

---
+++ PersistentArray/SADatabase/DCUnitTest/CommunicationTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DCUnitTest
+{
+    [TestClass]
+    public class CommunicationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
